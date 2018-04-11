122643b5d5cc11a7f1993f395114859271ec37d6
Patrick <patrick@westberliner.net>
2018-04-12 00:42:12 +0200

    working on server folder structure and config

---
+++ roles/webserver-php/files/startserver.sh
@@ -0,0 +1,4 @@
+brew services start mongodb
+brew services start mariadb
+brew services start httpd
+brew services start dnsmasq
---
+++ roles/webserver-php/files/stopserver.sh
@@ -0,0 +1,4 @@
+brew services stop mongodb
+brew services stop mariadb
+brew services stop httpd
+brew services stop dnsmasq
